9165701f23bd50491c07800b674f7dc1b8fda0c1
Raju1-Ahmed <robiussaniraju@gmail.com>
2023-01-11 03:18:58 +0600

    singlepost done

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const path = require('path')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tlhmfbm.mongodb.net/?retryWrites=true&w=majority`;
-console.log("uri is connect:", uri);
+// console.log("uri is connect:", uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -18,28 +22,45 @@ async function run() {
     //Data Base File Collection
     const postsCollection = client.db('Post-Collection').collection('post');
 
-    //user collect api for Get
+    //post collect api for Get
     app.get('/posts', async (req, res) => {
       const post = await postsCollection.find().toArray();
       res.send(post);
     });
 
+    //post collect api for Get single id
+    app.get('/service/:id',  async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId (id) };
+      const service = await postsCollection.findOne(query);
+      res.send(service);
+    })
+
     //user collect api for post
-    // app.post('/addUser', async (req, res) => {
-    //   const review = req.body;
-    //   const result = await usersCollection.insertOne(review);
-    //   res.send(result);
-    // })
-      // delete user collection api 
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) =>{
+        cb(null, "images");
+      },
+      filename: (req, file,cb) =>{
+        cb(null, req.body.name);
+      }
+    })
+    const upload = multer({storage: storage});
+    app.post('/upload', upload.single("file"), (req, res) => {
+     console.log(req.file);
+     if(!req.file){
+        res.send({ code: 500, msg: 'err' })
+     } else{
+      res.send({ code: 200, msg: 'upload successfully s' })
+     }
+    })
 
-    // app.delete('/order/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await usersCollection.deleteOne(query);
-    //   res.send(result);
-    // })
-
-    
+     //user collect api for post
+     app.post('/post', async (req, res) => {
+      const review = req.body;
+      const result = await postsCollection.insertOne(review);
+      res.send(result);
+    })
 
   } finally {
   }
